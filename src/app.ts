d2919c686ad6c1b71271eb175fe6468c815ad71a
Chan Kang <me@kentakang.com>
2023-05-25 14:39:32 +0900

    feat: add react / vite configurations

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import { app, BrowserWindow } from 'electron';
 
 let window: BrowserWindow | null = null;
+const stage: 'PROD' | 'DEV' = (process.env.STAGE as 'PROD' | 'DEV');
 
 const createWindow = () => {
   window = new BrowserWindow({
     width: 800,
     height: 600,
   });
+
+  if (stage === 'DEV') {
+    window.loadURL('http://localhost:5173');
+  } else {
+    window.loadFile('frontend/index.html');
+  }
 };
 
 app.whenReady().then(() => {
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: `${process.cwd()}/src/frontend`,
+  plugins: [react()],
+  build: {
+    outDir: '../../dist/frontend',
+  },
+});
